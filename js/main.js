8da4b14de88d1aff1ee7c3338b9843600a0d3a98
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2023-10-25 20:58:00 +0500

    pages done

--- js/main.js
+++ js/main.js
@@ -16,3 +16,19 @@ let modeBtn = document.getElementById("dark-light");
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
